b931f1bfc75aa72f7f65ee9cf88d95997bd0223e
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-13 08:46:23 -0600

    Se imprime informacion de variables del usuario en la pantalla

--- HelloWorldPlatzi/HelloWorldPlatzi/Program.cs
+++ HelloWorldPlatzi/HelloWorldPlatzi/Program.cs
@@ -7,6 +7,12 @@ namespace HelloWorldPlatzi
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Platzi!");
+
+            int height = 165;
+            int age = 22;
+            string name = "Eduardo Peraza";
+            string information = "The requested info is: \n User name: " + name  + "\n User age: " + age + "\n User height: " + height;
+            Console.WriteLine(information);    
         }
     }
 }
