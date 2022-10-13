4480bc84f257310d6b3f1a00e14c53a29fd14b3f
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-13 10:00:37 -0600

    Ejemplo de operacion con float

---
+++ SquareArea/SquareArea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program calculate the area of a rectangle
+
+
+            //rectangles values
+            float sideA = 10.5f;
+            float sideB = 20;
+
+            // rectangule area formula is a*b
+            float area = sideA * sideB;
+
+            Console.WriteLine("The area of the rectangle with height: " + sideA +"cm and lenght: " + sideB + "cm is: " + area + "cm.");    
+
+        }   
+    }
+}
