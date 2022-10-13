41b3acbe8a8600ce7d14c59e6dadedc839a00a5f
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-13 08:35:05 -0600

    Ejemplo de string, solicitud e impresion de nombre en pantalla

---
+++ StringExample/StringExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please write your full name and press ENTER:");
+            string fullUserName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + fullUserName + ", welcome to Platzi!");
+        }
+    }
+}
