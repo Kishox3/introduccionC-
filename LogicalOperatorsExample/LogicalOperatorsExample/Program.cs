71da5395f2df8cad3ca263e214135ee4e88a13f9
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-13 10:08:12 -0600

    ejemplo con &&

---
+++ LogicalOperatorsExample/LogicalOperatorsExample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool value1 = true;
+bool value2 = true;
+bool value3 = false;
+
+// && , || , !
+// and , or , not
+
+bool result = value1 && value2 && value3;
+Console.WriteLine("The result of the logical AND is: " + result);
