d76dce1b77ca196e87e9228734196f423cb7c25e
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-13 10:10:44 -0600

    ejemplo con ! y los demas operadores tambien agregados

--- LogicalOperatorsExample/LogicalOperatorsExample/Program.cs
+++ LogicalOperatorsExample/LogicalOperatorsExample/Program.cs
@@ -8,5 +8,11 @@ bool value3 = false;
 // && , || , !
 // and , or , not
 
-bool result = value1 || value2 || value3;
-Console.WriteLine("The result of the logical AND is: " + result);
+bool result1 = value1 || value2 || value3;
+Console.WriteLine("The result of the logical AND is: " + result1);
+
+bool result2 = value1 && value2 && value3;
+Console.WriteLine("The result of the logical AND is: " + result2);
+
+bool result3 = !value1; //el not es lo inverso al valor booleano
+Console.WriteLine("The result of the logical AND is: " + result3);
