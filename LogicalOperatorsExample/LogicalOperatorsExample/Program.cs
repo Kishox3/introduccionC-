4b3c24e9bfe99f1b4e59b78f0ffc07e2441842cd
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-13 20:11:23 -0600

    Se agrega ejemplo de operadores relaciones

--- LogicalOperatorsExample/LogicalOperatorsExample/Program.cs
+++ LogicalOperatorsExample/LogicalOperatorsExample/Program.cs
@@ -16,3 +16,33 @@ Console.WriteLine("The result of the logical AND is: " + result2);
 
 bool result3 = !value1; //el not es lo inverso al valor booleano
 Console.WriteLine("The result of the logical AND is: " + result3);
+
+//operadores relacionales
+
+
+int val1 = 3;
+int val2 = 18;
+int val3 = -18;
+
+bool results1 = val1 == val2;
+Console.WriteLine("The result of value 1 is: " + results1);
+
+bool results2 = val2 > val3;
+Console.WriteLine("The result of value 2 is: " + results2);
+
+bool results3 = val3 < val1;
+Console.WriteLine("The result of value 3 is: " + results3);
+
+bool results4 = val2 != val3;
+Console.WriteLine("The result of value 4 is: " + results4);
+
+bool results5 = val3 >= val1;
+Console.WriteLine("The result of value 5 is: " + results5);
+
+bool results6 = val3 <= val1;
+Console.WriteLine("The result of value 6 is: " + results6);
+
+
+
+
+
