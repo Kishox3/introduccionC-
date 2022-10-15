7e3e2cfbef62ecdda673839f5e41b830192e4d5a
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-15 10:21:41 -0600

    Ejemplo de arays - cafes

---
+++ ArrayDemoProject/ArrayDemoProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayDemoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cofeeTypes;
+            float[] cofeeValues;
+
+            cofeeTypes =  new string[] {"Espresso", "Latte", "Capuccino", "Americano"};
+            cofeeValues = new float[] { 1.2f, 2.5f, 2.5f, 1.2f};
+
+            Console.WriteLine("-- Precios de cafes --");
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine("*" + cofeeTypes[i] + " - $" + cofeeValues[i]);  
+        }
+    }
+}
--- SquareArea/SquareArea/Program.cs
+++ SquareArea/SquareArea/Program.cs
@@ -8,21 +8,24 @@ namespace SquareArea
         {
             // This program calculate the area of a rectangle
 
+            Console.WriteLine("Insert the height in cm: ");
+            float sideA = float.Parse(Console.ReadLine());      //se convierte a un tipo flotando con float.Parse
 
-            //rectangles values
-            float sideA = 10.5f;
-            int sideB = 20;
+            Console.WriteLine("Insert the height in cm: ");
+            float sideB = float.Parse(Console.ReadLine());
+
+            // rectangule area formula is a*b
+            float area = sideA * sideB;
+            Console.WriteLine("The area of the rectangle with height: " + sideA +"cm and lenght: " + sideB + "cm is: " + area + "cm.");
+
+            /*
             sideB++; //INCREMENTA LA VARIABLE DE 1 EN 1 CADA VEZ QUE SE EJECUTA
             sideB++;
             sideB++;
             sideB++;
             sideB--;
+            */
 
-            // rectangule area formula is a*b
-            float area = sideA * sideB;
-
-            Console.WriteLine("The area of the rectangle with height: " + sideA +"cm and lenght: " + sideB + "cm is: " + area + "cm.");    
-
-        }   
+        }
     }
 }
