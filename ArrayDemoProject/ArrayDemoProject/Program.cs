31723ce9ad1d243ea98f6379cc8e9a4a9416fca5
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-15 21:08:22 -0600

    Se usa este proyecto para explicar metodos Se explora el metodo Random

--- ArrayDemoProject/ArrayDemoProject/Program.cs
+++ ArrayDemoProject/ArrayDemoProject/Program.cs
@@ -12,6 +12,8 @@ namespace ArrayDemoProject
             cofeeTypes =  new string[] {"Espresso", "Latte", "Capuccino", "Americano"};
             cofeeValues = new float[] { 1.2f, 2.5f, 2.5f, 1.2f};
 
+            cofeeTypes[1] = "Lungo"; //con esto se puede variar un valor especifico del array
+
             Console.WriteLine("-- Precios de cafes --");
             for (int i = 0; i < 4; i++)
                 Console.WriteLine("*" + cofeeTypes[i] + " - $" + cofeeValues[i]);  
---
+++ ListExampleProject/ListExampleProject/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string> tacoShoppingList = new List<string>();
+
+tacoShoppingList.Add("Cinco tacos de suadero");
+tacoShoppingList.Add("Cuatro tacos des tripa");
+tacoShoppingList.Add("Cinco tacos de pastor");
+tacoShoppingList.Add("Cuatro Coca-Cola");
+
+for (int i = 0; i < tacoShoppingList.Count; i++)
+{
+    Console.WriteLine(tacoShoppingList[i]);
+}
+
+Console.WriteLine("------------------");
+
+
+tacoShoppingList.Remove("Cinco tacos de suadero"); //las listas buscan y comparan, no se tiene que usar posiciones como los arrays
+tacoShoppingList.RemoveAt(0); //pero tambien puede funcionar por posicion
+
+for (int i = 0; i < tacoShoppingList.Count; i++)
+{
+    Console.WriteLine(tacoShoppingList[i]);
+}
