bd8da01f3adcdb27df89639d32b0e91cf88fbc2c
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-16 22:47:01 -0600

    ejemplo if y tambien else

---
+++ IfStatement/IfStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IfStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int anyValue = 7;
+            string message = string.Empty;
+
+            if (anyValue == 6)
+            {
+                message = "OMG are you crazy?";
+            }
+            else
+            {
+                message = "Bad luck bro";
+            }
+            Console.WriteLine($"The Answer is: {message}");
+
+        }
+    }
+}
