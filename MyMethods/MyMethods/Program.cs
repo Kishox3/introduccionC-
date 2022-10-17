1eecc34118ed3b58e907f386f614b3f22a608be5
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-16 22:39:56 -0600

    solucion reto de la clase y creacion de metodos custom

---
+++ MyMethods/MyMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyMethods
+{
+    internal class Program
+    {
+        public void IntegerAddition(int a, int b) //metodo creado, es de tipo void entonces no retorna valor
+        {
+            int Addition = a + b;
+            Console.WriteLine("El resultado es " + Addition);
+        }
+
+        public int IntegerMultiplication(int a, int b) //metodo creado, es de tipo int entonces almacena valores enteros
+        {
+            int Multiplication = a * b;
+            return Multiplication;
+        }
+
+        public float FloatDivision(float a, float b) //solucion reto
+        {
+            float Division = a / b;
+            return Division;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program MyProgram = new Program();
+            MyProgram.IntegerAddition(3, 6);
+
+            int resultMultiplication = MyProgram.IntegerMultiplication(12, 2);
+            Console.WriteLine("El resultado de la multiplicación es " + resultMultiplication);
+
+            float resultDivision = MyProgram.FloatDivision(25f, 6.6f);
+            Console.WriteLine("El resultado de la division es " + resultDivision); //solucion reto platzi
+        }
+    }
+}
