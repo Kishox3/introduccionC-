d961bbc2f97d0bb671edfdfcec2a2938be9959c8
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-17 21:43:19 -0600

    for example

---
+++ ForCycle/ForCycle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ForCycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciclo For ejecuta instrucciones en cada iteracion (cada vez que se ejecuta un codigo)
+
+            for (int i = 0; i <= 50; i = i + 10)
+            {
+                Console.WriteLine("Hello World #"+i);
+            } 
+                
+        }
+    }
+}
