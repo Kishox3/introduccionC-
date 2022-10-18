9bda39d8140b75ceba6c771edd3dca5ad9d5da22
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-17 21:52:42 -0600

    ejemplo ciclo while

---
+++ WhileCycle/WhileCycle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WhileCycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueSoftwareExecution = true;
+
+            while (continueSoftwareExecution == true)
+            {
+                Console.WriteLine("Quieres detener el programa: Y/N");
+                string response = Convert.ToString(Console.ReadLine());
+                if (response == "Y" || response == "y")
+                {
+                    continueSoftwareExecution = false;
+                }
+            }
+        }
+    }
+}
